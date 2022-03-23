517bdba898f07b30ebd96287423151e20d54d525
Zuvix <samuelkubala@gmail.com>
2022-03-23 19:40:44 +0100

    adding additional tests

--- 1.py
+++ 1.py
@@ -12,7 +12,8 @@ roman_dict = {
     "M": 1000
 }
 
-#Function to check if there is a forbidden repetition of characters or bad count of characters
+
+# Function to check if there is a forbidden repetition of characters or bad count of characters
 def checkMoreThanThree(romanNumber):
     key_list = list(roman_dict.keys())
 
@@ -20,7 +21,7 @@ def checkMoreThanThree(romanNumber):
         sum = 0
         maxSum = 1
 
-        if x%2 == 0:
+        if x % 2 == 0:
             maxSum = 3
 
         for element in romanNumber:
@@ -29,44 +30,49 @@ def checkMoreThanThree(romanNumber):
 
                 if sum > maxSum:
                     return False
+            else:
+                sum = 0
     return True
 
-#Check for invalid characters
+
+# Check for invalid characters
 def validateRomanElements(romanNumber):
     for element in romanNumber:
         if not (element in roman_dict):
             return False
     return True
 
-#Check if number is in correct order
+
+# Check if number is in correct order
 def correctNumberOrder(element, max, usedNumbers_list):
     for item in usedNumbers_list:
         if roman_dict[element] >= roman_dict[item]:
             return False
-        
-    if max == "I" and (element in ["L","C","D","M"]):
+
+    if max == "I" and (element in ["L", "C", "D", "M"]):
         return False
-    if max == "V" and (element in ["X","L","C","D","M"]):
+    if max == "V" and (element in ["X", "L", "C", "D", "M"]):
         return False
-    if max == "X" and (element in ["D","M"]):
+    if max == "X" and (element in ["D", "M"]):
         return False
-    if max == "L" and (element in ["C","D","M"]):
+    if max == "L" and (element in ["C", "D", "M"]):
         return False
     if max == "D" and element == "M":
         return False
-    
+
     return True
 
-#Main function to convert roman to INT
+
+# Main function to convert roman to INT
 def convertToInt(romanNumber):
-    romanNumber = romanNumber.strip() # remove leading and trailing whitespaces
+    romanNumber = romanNumber.strip()  # remove leading and trailing whitespaces
 
     if len(romanNumber) == 0:
         return -9999
 
     if validateRomanElements(romanNumber) == False:
         return -9999
-    
+
     if checkMoreThanThree(romanNumber) == False:
         return -9999
 
@@ -75,26 +81,27 @@ def convertToInt(romanNumber):
     max = None
     repetition = False
     usedNumbers_list = []
+    subtractableNumbers_list = []
     prevElem = romanNumber[0]
 
     for element in romanNumber:
-        if i == 0: 
+        if i == 0:
             sum += roman_dict[element]
             max = element
             i += 1
             continue
 
         if correctNumberOrder(element, max, usedNumbers_list) == False:
-            return -9999      
+            return -9999
 
         if roman_dict[prevElem] < roman_dict[element]:
             if prevElem in ["V", "L", "D"]:
                 return -9999
-            if prevElem == "I" and not (element in ["V","X"]):
+            if prevElem == "I" and not (element in ["V", "X"]):
                 return -9999
-            if prevElem == "X" and not (element in ["L","C"]):
+            if prevElem == "X" and not (element in ["L", "C"]):
                 return -9999
-            if prevElem == "C" and not (element in ["D","M"]):
+            if prevElem == "C" and not (element in ["D", "M"]):
                 return -9999
             if repetition:
                 return -9999
@@ -103,89 +110,143 @@ def convertToInt(romanNumber):
                 max = element
 
             usedNumbers_list.append(element)
+            subtractableNumbers_list.append(prevElem)
             sum -= 2 * roman_dict[prevElem]
 
         elif prevElem == element:
             repetition = True
         elif (roman_dict[prevElem] > roman_dict[element]) and repetition:
             repetition = False
+        elif element in subtractableNumbers_list:
+            return -9999
 
         sum += roman_dict[element]
         prevElem = element
- 
+
     if sum > 3999:
         return -9999
     return sum
 
 
-#TEST FUNCTIONS
+def check_actual_max_number(romanNumber):
+    max_value = 1000
+    for i in range(0, len(romanNumber)):
+        next_element = ""
+        if(i+1 < len(romanNumber)):
+            next_element = romanNumber[i+1]
+        if roman_dict[romanNumber[i]] > max_value:
+            return -9999
+        if roman_dict[romanNumber[i]] < max_value:
+            # check if the next value is substractible canSubstract funkciu sprav
+            max_value = roman_dict[romanNumber[i]]
+
+
+# TEST FUNCTIONS
 def test1():
     assert convertToInt("I") == 1, "Test1, should be 1"
-   
+
+
 def test2():
     assert convertToInt("V") == 5, "Test2, should be 5"
-   
+
+
 def test3():
     assert convertToInt("-I") == -9999, "Test3, should be -9999"
+
 
 def test4():
     assert convertToInt("X") == 10, "Test4, should be 10"
 
+
 def test5():
     assert convertToInt("III") == 3, "Test5, should be 3"
-   
+
+
 def test6():
     assert convertToInt("MMMM") == -9999, "Test6, should be -9999"
+
 
 def test7():
     assert convertToInt("IIII") == -9999, "Test7, should be -9999"
 
+
 def test8():
     assert convertToInt("DD") == -9999, "Test8, should be -9999"
+
 
 def test9():
     assert convertToInt("IV") == 4, "Test9, should be 4"
 
+
 def test10():
     assert convertToInt("VX") == -9999, "Test10, should be -9999"
+
 
 def test11():
     assert convertToInt("LC") == -9999, "Test11, should be -9999"
 
+
 def test12():
     assert convertToInt("DM") == -9999, "Test12, should be -9999"
+
 
 def test13():
     assert convertToInt("IL") == -9999, "Test13, should be -9999"
 
+
 def test14():
     assert convertToInt("IIV") == -9999, "Test14, should be -9999"
+
 
 def test15():
     assert convertToInt("MXXXIV") == 1034, "Test15, should be 1034"
 
+
 def test16():
     assert convertToInt("MCMLXXXVIII") == 1988, "Test16, should be 1988"
 
+
 def test17():
-    assert convertToInt("") == -9999, "Test17, should be -9999" 
+    assert convertToInt("") == -9999, "Test17, should be -9999"
+
 
 def test18():
     assert convertToInt("  CXIX ") == 119, "Test18, should be 119"
 
+
 def test19():
     assert convertToInt("CMCM") == -9999, "Test19, should be -9999"
+
 
 def test20():
     assert convertToInt("CMM") == -9999, "Test20, should be -9999"
 
+
 def test21():
     assert convertToInt("DCM") == -9999, "Test21, should be -9999"
+
 
 def test22():
     assert convertToInt("XCIX") == 99, "Test22, should be 99"
 
-#RUN TESTS
+
+def test23():
+    assert convertToInt("MMMCM") == 3900, "Test23 should be 3900"
+
+
+def test24():
+    assert convertToInt("CDC") == -9999, "Test24 should be -9999"
+
+
+def test25():
+    assert convertToInt("IXIV") == -9999, "Test25 should be -9999"
+
+
+def test26():
+    assert convertToInt("XXXVIX") == -9999, "Test26 should be -9999"
+
+
+# RUN TESTS
 test1()
 test2()
 test3()
@@ -208,3 +269,7 @@ test19()
 test20()
 test21()
 test22()
+test23()
+test24()
+test25()
+test26()
