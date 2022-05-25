e5603d83a6c901af0abedff5ba13eaa4afffd7fa
Filip-Novak <51423248+Filip-Novak@users.noreply.github.com>
2022-05-25 15:02:43 +0200

    Update tests2.py

--- Skuska/tests2.py
+++ Skuska/tests2.py
@@ -10,60 +10,60 @@ class Testy_uloha2(unittest.TestCase):
         #self.assertEqual(roman.romanToNumber("IVXLCDM", "I"(self)::, 1(self)::
 
     def test_IVXLCDM_1_Zero(self):
-        roman = sk.RomanNumber()
+        roman = sk.RomanNumberFull()
         self.assertEqual(roman.maximum(), 3999)
         self.assertEqual(roman.minimum(), -3999)
 
     def test_OI(self): 
-        roman = sk.RomanNumber("OI")
+        roman = sk.RomanNumberFull("OI")
         self.assertEqual(roman.maximum(), 3)
         self.assertEqual(roman.minimum(), -3)
 
     def test_O(self): 
-        roman = sk.RomanNumber("O")
+        roman = sk.RomanNumberFull("O")
         self.assertEqual(roman.maximum(), 3999)
         self.assertEqual(roman.minimum(), -3999)
 
     def test_ABCDEFGHIJKLMN(self): 
-        roman = sk.RomanNumber("ABCDEFGHIJKLMN")
+        roman = sk.RomanNumberFull("ABCDEFGHIJKLMN")
         self.assertEqual(roman.maximum(), 3999999)
         self.assertEqual(roman.minimum(), -3999999)
 
     def test_IVXLCDMPQRSTUWYZ_53864324_Zero(self): 
-        roman = sk.RomanNumber("OIVXLCDMPQRSTUWYZ")
+        roman = sk.RomanNumberFull("OIVXLCDMPQRSTUWYZ")
         self.assertEqual(roman.maximum(), 89999999)
         self.assertEqual(roman.minimum(), -89999999)
 
 
     def test_IVXLCDM_3896(self): 
-        roman = sk.RomanNumber()
+        roman = sk.RomanNumberFull()
         roman.setValue(-3896)
         self.assertEqual(roman.getValue(), -3896)
 
     def test_Zero(self): 
-        roman = sk.RomanNumber()
+        roman = sk.RomanNumberFull()
         self.assertTrue(roman.setValue(0))
         self.assertEqual(roman.getValue(), 0)
 
     def test_IVXLCDM_NO_NUMBER_MAX_Zero(self): 
-        roman = sk.RomanNumber()
+        roman = sk.RomanNumberFull()
         self.assertFalse(roman.setValue(4000))
         self.assertEqual(roman.getValue(), 0)
 
     def test_I_I_5(self): 
-        roman = sk.RomanNumber("I")
+        roman = sk.RomanNumberFull("I")
         roman.setValue(5)
         self.assertEqual(roman.getValue(), 5)
 
     def test_IVXLCDMPQRSTUWYZ_53864324(self): 
-        roman = sk.RomanNumber("OIVXLCDMPQRSTUWYZ")
+        roman = sk.RomanNumberFull("OIVXLCDMPQRSTUWYZ")
         roman.setValue(53864324)
         self.assertEqual(roman.getValue(), 53864324)
 
 
     def test_TestEfektivnosti_Zero(self):
         for i in range(1000):
-            roman = sk.RomanNumber("OIVXLCDMPQRSTUWY")
+            roman = sk.RomanNumberFull("OIVXLCDMPQRSTUWY")
             roman.setValue(16353048)
             if (roman.getValue() != 16353048): 
                 self.assertTrue(False)
