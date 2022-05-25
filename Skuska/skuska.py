7156ef0bfc4ba26c5e4a16c292fd3727a27521fa
Zuvix <samuelkubala@gmail.com>
2022-05-25 14:36:00 +0200

    Update skuska.py

--- Skuska/skuska.py
+++ Skuska/skuska.py
@@ -7,37 +7,39 @@ DEFINED_ZERO_ALPHABET = "OIVXLCDM"
 def check_alphabet(romanLetters, hasZero):
 
     if hasZero:
-        alphabet = DEFINED_ZERO_ALPHABET
+        default_alphabet = DEFINED_ZERO_ALPHABET
     else:
-        alphabet = DEFINED_ALPHABET
+        default_alphabet = DEFINED_ALPHABET
     if (str != type(romanLetters)):
-        return alphabet
+        return default_alphabet
 
     romanLetters = romanLetters.strip()
 
     if (len(romanLetters) == 0):
-        return alphabet
+        return default_alphabet
 
     if roman.validate_roman_letters(romanLetters) == False:
-        return alphabet
+        return default_alphabet
 
     if roman.check_roman_letters_for_unique_letters(romanLetters) == False:
-        return alphabet
+        return default_alphabet
+
+    return romanLetters
 
 
 class RomanNumber:
     def __init__(self, romanLetters):
-        self.romanLetters = check_alphabet(romanLetters, False)
+        self.alphabet = check_alphabet(romanLetters, False)
         self.value = 0
 
     def romanLetters(self):
-        return self.romanLetters
+        return self.alphabet
 
     def maxNumber(self):
-        return maxRoman.maxNumberFromRomanLetters(self.romanLetters)
+        return maxRoman.maxNumberFromRomanLetters(self.alphabet)
 
     def setValue(self, value):
-        if value > self.maxNubmer() or value <= 0:
+        if value > self.maxNumber() or value <= 0:
             return False
         self.value = value
         return True
@@ -46,7 +48,7 @@ class RomanNumber:
         return self.value
 
     def setRomanNumber(self, romanNumber):
-        roman.set_roman_letters(self.romanLetters)
+        roman.set_roman_letters(self.alphabet)
         testNumber = roman.convert_to_int(romanNumber)
         if(testNumber == roman.INCORRECT_NUMBER):
             return False
@@ -54,29 +56,31 @@ class RomanNumber:
         return True
 
     def getRomanNumber(self):
-        roman.set_roman_letters(self.romanLetters)
+        roman.set_roman_letters(self.alphabet)
         return roman.convert_to_int(self.value)
 
 
 class RomanNumberFull:
     # Public methodes and Constructor
     def __init__(self, romanLetters):
-        self.romanLetters = self.check_alphabet(romanLetters, True)
+        self.alphabet = check_alphabet(romanLetters, True)
         self.value = 0
         self.zeroChar = romanLetters[0]
-        self.romanLetters = self.romanLetters[1:]
+        self.alphabet = self.romanLetters[1:]
 
     def maximum(self):
-        return maxRoman.maxNumberFromRomanLetters(self.romanLetters)
+        return maxRoman.maxNumberFromRomanLetters(self.alphabet)
 
-    def mimnimum(self):
-        return -1*maxRoman.maxNumberFromRomanLetters(self.romanLetters)
+    def minimum(self):
+        return -1*maxRoman.maxNumberFromRomanLetters(self.alphabet)
 
     def setvalue(self, value):
-        if value > self.maximum() or value < self.mimnimum():
+        if value > self.maximum() or value < self.minimum():
             return False
         self.value = value
         return True
 
 
-x = RomanNumber("l")
+x = RomanNumber("IS")
+print(x.maxNumber())
+print(x.romanLetters())
